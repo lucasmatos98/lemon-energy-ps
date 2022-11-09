e055092d7238e0981b1f4b0809aea653fc8181f2
Lucas Matos <negocios.matos@gmail.com>
2022-11-09 16:00:40 -0300

    feat: example added

--- index.ts
+++ index.ts
@@ -97,6 +97,27 @@ function getIneligibilityReasons({ consumptionTypeEligible, tariffModalityEligib
 const example1 = {
 	"numeroDoDocumento": "14041737706",
 	"tipoDeConexao": "bifasico",
+	"classeDeConsumo": "comercial",
+	"modalidadeTarifaria": "convencional",
+	"historicoDeConsumo": [
+		3878, // mes atual
+		9760, // mes anterior
+		5976, // 2 meses atras
+		2797, // 3 meses atras
+		2481, // 4 meses atras
+		5731, // 5 meses atras
+		7538, // 6 meses atras
+		4392, // 7 meses atras
+		7859, // 8 meses atras
+		4160, // 9 meses atras
+		6941, // 10 meses atras
+		4597  // 11 meses atras
+	]
+}
+
+const example2 = {
+	"numeroDoDocumento": "14041737706",
+	"tipoDeConexao": "bifasico",
 	"classeDeConsumo": "rural",
 	"modalidadeTarifaria": "verde",
 	"historicoDeConsumo": [
@@ -114,3 +135,4 @@ const example1 = {
 }
 
 console.log(getReport(example1));
+console.log(getReport(example2));
